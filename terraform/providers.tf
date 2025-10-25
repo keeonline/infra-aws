a0aaed34ccacfd86d4a8352a530c90dc37a6ae95
Matt Keeble <48411999+keeonline@users.noreply.github.com>
2025-10-25 14:39:45 +0100

    Feature tf (#4)
    * added initial terraform code
    
    * fixed syntax error
    
    * updated gh-actions version ref

---
+++ terraform/providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "5.87.0"
+    }
+  }
+}
+
+provider "aws" {
+    default_tags {
+      tags = {
+        InfraEnvironment = "${var.infra_environment}"
+        IacVersion       = "${var.infra_version}"
+        IacRepo          = "${var.infra_repo}"
+      }
+    }
+}
